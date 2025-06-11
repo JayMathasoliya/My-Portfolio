8041edbede2c0b2b833a0bb06253f5222557c43a
Jay Mathasoliya <pateljay08122002@gmail.com>
2025-06-11 08:52:17 +0530

    Feat: Complete the Work Section

--- src/app/components/work/WorkItem.tsx
+++ src/app/components/work/WorkItem.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Badge } from "@/components/ui/badge";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { FiArrowDownRight } from "react-icons/fi";
 
-const WorkItem = () => {
+type WorkItemProps = {
+  href: string;
+  category: string;
+  img: string;
+  title: string;
+};
+
+const WorkItem = ({ href, category, img, title }: WorkItemProps) => {
   return (
-    <div>WorkItem</div>
-  )
-}
+    <Link href={href} className="group">
+      <div className="w-full h-[300px] p-8 rounded-[30px] flex items-center justify-center mb-6 relative overflow-hidden bg-[#F4F4F4]">
+        <Badge className="bg-primary text-sm z-40 absolute top-6 left-6 capitalize">
+          {category}
+        </Badge>
+        <Image
+          src={img}
+          fill
+          priority
+          quality={10}
+          className="object-cover group-hover:scale-105 transition-all duration-500"
+          alt={title}
+        />
+      </div>
+      <div className="flex items-center justify-center">
+        <div className="flex-1">
+          <h3 className="text-sm sm:text-base font-semibold text-secondary-foreground">
+            {title}
+          </h3>
+        </div>
+        <button className="bg-accent text-white rounded-full w-[40px] h-[40px] flex items-center justify-center -rotate-90 group-hover:-rotate-45 transition-all duration-500">
+          <FiArrowDownRight className="text-2xl" />
+        </button>
+      </div>
+    </Link>
+  );
+};
 
-export default WorkItem
+export default WorkItem;
--- src/app/components/work/index.tsx
+++ src/app/components/work/index.tsx
@@ -1,9 +1,121 @@
-import React from "react";
+import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import AnimatedText from "../AnimatedText";
+import WorkItem from "./WorkItem";
+
+const data = [
+  {
+    href: "",
+    category: "fullstack",
+    img: "/assets/work/thumb-1.png",
+    title: "shopAll | E-commerce Website",
+  },
+  {
+    href: "",
+    category: "fullstack",
+    img: "/assets/work/thumb-1.png",
+    title: "MySocialWeb | Facebook Clone",
+  },
+  {
+    href: "",
+    category: "frontend",
+    img: "/assets/work/thumb-1.png",
+    title: "NewsMonkey | News App",
+  },
+  {
+    href: "",
+    category: "fullstack",
+    img: "/assets/work/thumb-1.png",
+    title: "PDF Tools",
+  },
+  {
+    href: "",
+    category: "frontend",
+    img: "/assets/work/thumb-1.png",
+    title: "Dance Academy Website",
+  },
+  {
+    href: "",
+    category: "fullstack",
+    img: "/assets/work/thumb-1.png",
+    title: "Track For Safe Admin Panel",
+  },
+];
 
 const Work = () => {
+  const uniqueCategories = Array.from(
+    new Set(data.map((item) => item.category))
+  );
+
+  const tabData = [
+    { category: "all" },
+    ...uniqueCategories.map((category) => ({ category })),
+  ];
+
+  const [tabValue, setTabValue] = useState("all");
+  const [visibleItems, setVisibleItems] = useState(6);
+
+  const filterWork =
+    tabValue === "all"
+      ? data.filter((item) => item.category !== "all")
+      : data.filter((item) => item.category === tabValue);
+
+  const handleMoreItems = () => {
+    setVisibleItems((prev) => prev + 2);
+  };
   return (
-    <section className="bg-blue-200 py-96" id="work">
-      Work
+    <section className="pt-24 min-h-[1000px]" id="work">
+      <div className="container mx-auto">
+        <Tabs defaultValue="all" className="w-full flex flex-col">
+          <div className="flex flex-col xl:flex-row items-center xl:items-start xl:justify-between mb-[30px]">
+            <AnimatedText
+              text="My Latest Work"
+              textStyles="h2 mb-[30px] xl:mb-0"
+            />
+
+            <TabsList className="max-w-max h-full mb-[30px] flex flex-col md:flex-row gap-4 md:gap-0">
+              {tabData.map((item, index) => {
+                return (
+                  <TabsTrigger
+                    key={index}
+                    value={item.category}
+                    className="capitalize w-[120px]"
+                    onClick={() => setTabValue(item.category)}
+                  >
+                    {item.category}
+                  </TabsTrigger>
+                );
+              })}
+            </TabsList>
+          </div>
+
+          <TabsContent value={tabValue} className="w-full">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-[30px]">
+              <AnimatePresence>
+                {filterWork.slice(0, visibleItems).map((item, index) => (
+                  <motion.div
+                    key={index}
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.3 }}
+                  >
+                    <WorkItem {...item} />
+                  </motion.div>
+                ))}
+              </AnimatePresence>
+            </div>
+
+            {visibleItems < filterWork.length && (
+              <div className="flex justify-center mt-12">
+                <button className="btn btn-accent cursor-pointer" onClick={handleMoreItems}>
+                  Load More
+                </button>
+              </div>
+            )}
+          </TabsContent>
+        </Tabs>
+      </div>
     </section>
   );
 };
